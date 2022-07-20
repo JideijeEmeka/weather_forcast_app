89f45caedaa4d3447820390bd3bb5fc5820f91f6
Emeka Jideije <jideije.emeka@gmail.com>
2022-07-20 06:03:10 +0100

    Design a customized app loader

---
+++ lib/loader.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Widget loader() {
+  return const Center(child:
+  Padding(
+    padding: EdgeInsets.symmetric(horizontal: 30),
+    child: SizedBox(
+      height: 20,
+        width: 20,
+      child: CircularProgressIndicator(
+        color: Colors.white,
+      ),
+    ),
+  ),);
+}
